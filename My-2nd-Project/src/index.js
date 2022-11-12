2b3188df99c6df5a7fe7863fa254c204fc99a244
Aaron D. Angshu <115901868+aarondb124@users.noreply.github.com>
2022-11-13 05:33:27 +0600

    Movie App
    bug cleared

--- My-2nd-Project/src/index.js
+++ My-2nd-Project/src/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const x = 11;
-let text = "This is my first react ";
-if (x < 10) {
-  text = "Hello";
-}
-
-const myElement = <h1>{text}</h1> ;
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+reportWebVitals();
